23680c496f7e30389a3d0396b44678cd80fd9829
hüseyin cangür <huseyincangur35@gmail.comn>
2022-04-15 16:11:40 +0300

    Push

---
+++ UseMagamentAPI/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using UseMagamentAPI.Models;
+
+namespace UseMagamentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+
+        private List<User> _users = Fake.FakeData.GetUsers(200);
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _users;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user =_users.FirstOrDefault(x=>x.Id== id);
+            return user;
+        }
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var edituser =_users.FirstOrDefault(x=>x.Id==user.Id);
+            edituser.FirstName= user.FirstName;
+            edituser.LastName= user.LastName;
+            edituser.Address= user.Address;
+            return user;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var user=_users.FirstOrDefault(x=>x.Id==id);
+            _users.Remove(user);
+        }
+
+
+    }
+}
